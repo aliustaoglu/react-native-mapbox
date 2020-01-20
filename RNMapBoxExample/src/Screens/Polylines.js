48a4cf8c7dd00f673cf86650abe69bb100f5d8c8
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2020-01-21 12:55:57 +1300

    Android polylines implementation

--- RNMapBoxExample/src/Screens/Polylines.js
+++ RNMapBoxExample/src/Screens/Polylines.js
@@ -9,10 +9,49 @@ import {commonStyles} from './styles';
 export default class Polylines extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      polylines: [
+        {
+          id: 'poly1',
+          properties: {
+            name: 'AA',
+            lineWidth: 3.0,
+            lineColor: '#006b8f',
+            lineType: 'dash',
+          },
+          coordinates: [
+            [174, -36],
+            [173, -36],
+          ],
+        },
+      ],
+    };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const that = this;
+    setTimeout(() => {
+      that.setState({
+        polylines: [
+          that.state.polylines[0],
+          {
+            id: 'poly2',
+            properties: {
+              name: 'AA',
+              lineWidth: 3.0,
+              lineColor: '#d21430',
+            },
+            coordinates: [
+              [173.81167, -35.36706],
+              [174.55874, -36.52328],
+              [175.61343, -38.75019],
+              [175.52553, -40.61016]
+            ],
+          },
+        ],
+      });
+    }, 2000);
+  }
 
   render() {
     return (
@@ -27,45 +66,7 @@ export default class Polylines extends React.Component {
           }}
           style={StyleSheet.absoluteFillObject}
           onMapReady={() => console.log('onMapReady callback')}
-          polylines={[
-            {
-              id: 'poly1',
-              properties: {
-                name: 'AA',
-                lineWidth: 3.0,
-                lineColor: '#000000',
-                lineType: 'dash'
-              },
-              coordinates: [
-                [174, -36],
-                [173, -36],
-              ],
-            },
-            {
-              id: 'poly2',
-              properties: {
-                name: 'AA',
-                lineWidth: 3.0,
-                lineColor: '#ff0000'
-              },
-              coordinates: [
-                [174, -33],
-                [173, -32],
-              ],
-            },
-            {
-              id: 'poly3',
-              properties: {
-                name: 'AA',
-                lineWidth: 3.0,
-                lineColor: '#ff00ff'
-              },
-              coordinates: [
-                [170, -30],
-                [172, -31],
-              ],
-            }
-          ]}
+          polylines={this.state.polylines}
         />
         <View style={commonStyles.backButton}>
           <Button title="<Back" onPress={this.props.onGoBack} />
--- android/src/main/java/biz/aliustaoglu/mapbox/MapBoxModule/RNMBPolylines.java
+++ android/src/main/java/biz/aliustaoglu/mapbox/MapBoxModule/RNMBPolylines.java
@@ -1,26 +1,23 @@
 package biz.aliustaoglu.mapbox.MapBoxModule;
 
 import android.graphics.Color;
-
-import androidx.annotation.NonNull;
-
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.style.layers.LineLayer;
 import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.layers.PropertyValue;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-
+import com.mapbox.mapboxsdk.style.sources.Source;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RNMBPolylines {
 
     ReadableArray polylines;
+    String prefix = "RNMB";
 
     public RNMBPolylines(ReadableArray polylines) {
         this.polylines = polylines;
@@ -28,48 +25,105 @@ public class RNMBPolylines {
 
     public void update(MapBoxMapView mapBoxMapView) {
         Style style = mapBoxMapView.mapboxMap.getStyle();
-        if (style != null && style.isFullyLoaded()) {
+        if (style != null) {
+            while(!style.isFullyLoaded()) {}
             updatePolylines(style);
         }
     }
 
     private void updatePolylines(Style style) {
+        List<String> sourcesToRemove = getSourceIDs(style);
+
         for (int i = 0; i < this.polylines.size(); i++) {
+
             ReadableMap polyline = polylines.getMap(i);
             ReadableMap properties = polyline.getMap("properties");
             String id = properties.getString("id");
+            String sourceID = prefix + "-source-" + id;
 
-            // Construct optional varargs values for LineLayer.withProperties(...)
-            List<PropertyValue<?>> propertyValues = new ArrayList<PropertyValue<?>>();
+            GeoJsonSource source = (GeoJsonSource) style.getSource(sourceID);
 
-            propertyValues.add(PropertyFactory.lineJoin(Property.LINE_JOIN_MITER));
-            if (properties.hasKey("lineWidth"))
-                propertyValues.add(PropertyFactory.lineWidth((float) properties.getDouble("lineWidth")));
-            if (properties.hasKey("lineColor"))
-                propertyValues.add(PropertyFactory.lineColor(Color.parseColor(properties.getString("lineColor"))));
-            if (properties.hasKey("lineType")) {
-                if (properties.getString("lineType").equals("dash")) {
-                    propertyValues.add(PropertyFactory.lineDasharray(new Float[]{1f, 1f}));
-                }
+            if (source != null) {
+                updateSource(source, polyline);
+                sourcesToRemove.remove(sourceID);
+            } else {
+                addSourceAndLayer(id, polyline, properties, style);
             }
+        }
+        // If a polyline is removed from the props, remove it from the map as well
+        if (sourcesToRemove.size()>0) removeSources(sourcesToRemove, style);
 
-            PropertyValue<?>[] argsPropertyValues = propertyValues.toArray(new PropertyValue<?>[propertyValues.size()]);
+    }
 
-            String sourceId = "source" + id;
-            String lineId = "line" + id;
-            getJSON(polyline);
-
-            Feature feature = Feature.fromJson(getJSON(polyline));
-            style.addSource(new GeoJsonSource(sourceId, feature));
-
-
-            LineLayer lineLayer = new LineLayer(lineId, sourceId).withProperties(argsPropertyValues);
-            style.addLayer(lineLayer);
-
-
+    // Replace source with an empty feature
+    private void removeSources(List<String> sourceIDs, Style style){
+        for (int i=0;i<sourceIDs.size();i++){
+            GeoJsonSource source = (GeoJsonSource) style.getSource(sourceIDs.get(i));
+            // Empty feature
+            source.setGeoJson("{\n" +
+                    "  \"type\": \"Feature\",\n" +
+                    "  \"geometry\": {\n" +
+                    "    \"type\": \"LineString\",\n" +
+                    "    \"coordinates\": []\n" +
+                    "  }\n" +
+                    "}");
         }
     }
 
+    private List<String> getSourceIDs(Style style){
+        List<Source> sources = style.getSources();
+        List<String> sourceIDs = new ArrayList<>();
+        for(int i=0;i<sources.size();i++){
+            String id = sources.get(i).getId();
+            if (id.startsWith(prefix)) {
+                sourceIDs.add(id);
+            }
+        }
+        return sourceIDs;
+    }
+
+    private void updateSource(GeoJsonSource source, ReadableMap polyline){
+        source.setGeoJson(getJSON(polyline));
+    }
+
+    private void addSourceAndLayer(String id, ReadableMap polyline, ReadableMap properties, Style style){
+        String sourceId = prefix + "-source-" + id;
+        String lineId = prefix + "-line-" + id;
+        getJSON(polyline);
+
+        Feature feature = Feature.fromJson(getJSON(polyline));
+        style.addSource(new GeoJsonSource(sourceId, feature));
+
+        PropertyValue<?>[] propertyValues = getPropertyValues(properties);
+
+        LineLayer lineLayer = new LineLayer(lineId, sourceId).withProperties(propertyValues);
+        style.addLayer(lineLayer);
+    }
+
+    /**
+     * Construct optional varargs values for LineLayer.withProperties(...)
+     * @param properties Readable map of properties
+     * @return Property values
+     */
+    private PropertyValue<?>[] getPropertyValues(ReadableMap properties) {
+        List<PropertyValue<?>> propertyValues = new ArrayList<PropertyValue<?>>();
+
+        propertyValues.add(PropertyFactory.lineJoin(Property.LINE_JOIN_MITER));
+        if (properties.hasKey("lineWidth"))
+            propertyValues.add(PropertyFactory.lineWidth((float) properties.getDouble("lineWidth")));
+        if (properties.hasKey("lineColor"))
+            propertyValues.add(PropertyFactory.lineColor(Color.parseColor(properties.getString("lineColor"))));
+        if (properties.hasKey("lineType")) {
+            if (properties.getString("lineType").equals("dash")) {
+                propertyValues.add(PropertyFactory.lineDasharray(new Float[]{1f, 1f}));
+            }
+        }
+
+        PropertyValue<?>[] argsPropertyValues = propertyValues.toArray(new PropertyValue<?>[propertyValues.size()]);
+        return argsPropertyValues;
+    }
+
+    // ReadableMap.toString() adds an extra string. Remove it and get bare json.
     private String getJSON(ReadableMap readableMap) {
         String json = readableMap.toString();
         json = json.replaceAll("\\{ NativeMap: ", "");
