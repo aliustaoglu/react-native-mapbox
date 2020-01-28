c7f8264356fd5ac5a26ddd76e3bab8d93f4e92dd
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2020-01-28 16:44:32 +1300

    android - locate user functionality

--- android/src/main/java/biz/aliustaoglu/mapbox/MapBoxModule/MapBoxViewController.java
+++ android/src/main/java/biz/aliustaoglu/mapbox/MapBoxModule/MapBoxViewController.java
@@ -9,8 +9,12 @@ import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Nonnull;
@@ -22,6 +26,9 @@ public class MapBoxViewController extends SimpleViewManager<MapBoxMapView> {
     private MapboxMap mapboxMap;
     private ThemedReactContext context;
     private MapBoxMapView mapView;
+
+    // Native direct commands
+    private static final int LOCATE_USER = 0;
 
     @Nonnull
     @Override
@@ -48,6 +55,36 @@ public class MapBoxViewController extends SimpleViewManager<MapBoxMapView> {
                 .build();
     }
 
+    @Override
+    public Map<String, Integer> getCommandsMap() {
+        Map<String, Integer> commandsMap = new HashMap();
+        commandsMap.put("locateUser", LOCATE_USER);
+        return commandsMap;
+    }
+
+    @Override
+    public void receiveCommand(MapBoxMapView mapLayout, int commandId, @Nullable ReadableArray args) {
+        super.receiveCommand(mapLayout, commandId, args);
+    }
+
+    @Override
+    public void receiveCommand(MapBoxMapView mapLayout, String commandName, @Nullable ReadableArray args) {
+        super.receiveCommand(mapLayout, commandName, args);
+        switch (commandName) {
+            case "locateUser":
+                locateUser(mapLayout, args);
+                break;
+        }
+    }
+
+    private void locateUser(MapBoxMapView mapLayout, @Nullable ReadableArray args) {
+        Double lat = args.getMap(0).getDouble("latitude");
+        Double lng = args.getMap(0).getDouble("longitude");
+
+        mapLayout.mapboxMap.easeCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, lng)));
+    }
+
+
     @ReactProp(name = "camera")
     public void setCamera(MapBoxMapView mapBoxMapView, @Nullable ReadableMap camera) {
         mapBoxMapView.camera = new RNMBCamera(camera);
@@ -71,18 +108,19 @@ public class MapBoxViewController extends SimpleViewManager<MapBoxMapView> {
         if (mapBoxMapView.locationPicker == null) {
             mapBoxMapView.locationPicker = new RNMBLocationPicker(locationPicker, mapBoxMapView);
         } else {
-            if (mapBoxMapView.isMapReady) mapBoxMapView.locationPicker.update(mapboxMap, locationPicker);
+            if (mapBoxMapView.isMapReady)
+                mapBoxMapView.locationPicker.update(mapboxMap, locationPicker);
         }
     }
 
     @ReactProp(name = "markers")
-    public void setMarkers(MapBoxMapView mapBoxMapView, @Nullable ReadableArray markers){
+    public void setMarkers(MapBoxMapView mapBoxMapView, @Nullable ReadableArray markers) {
         mapBoxMapView.markers = new RNMBMarkers(markers);
         if (mapBoxMapView.isStyleLoaded) mapBoxMapView.setMarkers();
     }
 
     @ReactProp(name = "polylines")
-    public void setPolylines(MapBoxMapView mapBoxMapView, @Nullable ReadableArray polylines){
+    public void setPolylines(MapBoxMapView mapBoxMapView, @Nullable ReadableArray polylines) {
         mapBoxMapView.polylines = new RNMBPolylines(polylines);
         if (mapBoxMapView.isStyleLoaded) mapBoxMapView.setPolylines();
     }
--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-import React from "react";
-import { requireNativeComponent, Image, CameraRoll } from "react-native";
-import { mergePolylines } from "./lib/polylines";
+import React from 'react'
+import { requireNativeComponent, UIManager, findNodeHandle } from 'react-native'
+import { mergePolylines } from './lib/polylines'
 
-const mapRef = React.createRef();
+const moduleName = 'MapBoxViewController'
 
-const NativeMap = requireNativeComponent("MapBoxViewController", null);
+const NativeMap = requireNativeComponent(moduleName, null)
 
 class RNMBReactNativeMapbox extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
   mergeProps(props) {
-    let newProps = mergePolylines(props);
-    return newProps;
+    let newProps = mergePolylines(props)
+    return newProps
   }
 
   render() {
-    const merged = this.mergeProps(this.props);
-    return <NativeMap {...merged} />;
+    const merged = this.mergeProps(this.props)
+    return <NativeMap {...merged} ref={merged.mapRef} />
   }
 }
 
-export default RNMBReactNativeMapbox;
+const invokeNativeUIFunction = (moduleName, functionName, ref, params) => {
+  if (Platform.OS === 'ios') {
+    const func = NativeModules[moduleName][functionName]
+    func(params)
+  } else {
+    const handle = findNodeHandle(ref)
+    UIManager.dispatchViewManagerCommand(handle, functionName, params)
+  }
+}
+
+export const invokeMapFunction = (functionName, ref, params) => {
+  invokeNativeUIFunction(moduleName, functionName, ref, params)
+}
+
+export default RNMBReactNativeMapbox
